227d87cc76c26318f8a3d5af24135af001159be1
Gary Du <gudu9182@uni.sydney.edu.au>
2025-09-26 20:08:03 +0800

    :art: update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-PROXY_URL = "http://127.0.0.1:10808"
+PROXY_URL = None
 
 # 每次推送时最多展示的作品数量，当画师连投（检查时间间隔内发布多个独立作品）时生效，多图作品仅展示首图
 MAX_DISPLAY_WORKS = 3
@@ -8,4 +8,4 @@ MAX_DISPLAY_WORKS = 3
 # 注意: original质量的图片体积较大，可能导致发送失败
 IMAGE_QUALITY = 'large'
 
-CHECK_INTERVAL_HOURS = 1  # 检查更新的时间间隔，单位为小时
+CHECK_INTERVAL_HOURS = 3  # 检查更新的时间间隔，单位为小时
