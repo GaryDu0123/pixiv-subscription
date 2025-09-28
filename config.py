3e0b00b04e7a161f4a9199e741d25a97e569256a
Gary Du <gudu9182@uni.sydney.edu.au>
2025-09-29 01:58:40 +0800

    :art: update config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ PROXY_URL = None
 # 每次推送时最多展示的作品数量，当画师连投（检查时间间隔内发布多个独立作品）时生效，多图作品仅展示首图
 MAX_DISPLAY_WORKS = 3
 
-# 可选值: 'square_medium', 'medium', 'large', 'original'
+# 可选值: 'square_medium', 'medium', 'large', 'original' (可以大致理解为从小到大)
 # 注意: original质量的图片体积较大，可能导致发送失败
 IMAGE_QUALITY = 'large'
 
